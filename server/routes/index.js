f8ae6b67b3d71837b8f64138e23ac7dc2169b5b4
rdeguzman <ruthjf.16@gmail.com>
2022-10-30 23:11:29 -0400

    Added comment

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,3 +1,11 @@
+/*
+Filename: COMP229-F2020-MidTerm-Start
+Author's name: Ruth Jocelyn De Guzman
+Student's Name: Ruth Jocelyn De Guzman
+Student ID: 301224922
+Date: October 23, 2022
+*/
+
 // modules required for routing
 let express = require("express");
 let router = express.Router();
